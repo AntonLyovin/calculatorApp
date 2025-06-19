8de1468660a76279dab3be78dea876badb399a7f
Мещеряков Артём <ameshcheriakov@neoflex.ru>
2025-06-19 14:21:34 +0300

    fix

--- src/main/java/calculatorApp/calculator/CalculatorApplication.java
+++ src/main/java/calculatorApp/calculator/CalculatorApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
